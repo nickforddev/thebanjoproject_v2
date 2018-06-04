d3a74831681c812f62fc0921a270a0d0dd7d2d6f
Nick Ford <nickforddesign@gmail.com>
2018-06-04 11:29:07 -0400

    updated domains

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -1,4 +1,5 @@
 export default {
-  api: 'http://45.55.144.174/',
+  // api: 'http://45.55.144.174/',
+  api: 'http://wp.banjo2.emerson.edu/',
   mapbox: 'pk.eyJ1Ijoibmlja2ZvcmRkZXNpZ24iLCJhIjoiWlhxWDdUNCJ9.pnGwac6qU_VDgGfhGJ9EbQ'
 }
