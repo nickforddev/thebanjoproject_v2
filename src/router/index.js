f7fd09755c45030d33f15901c76c805cfb1433c4
Nick Ford <nickforddesign@gmail.com>
2019-09-28 12:13:54 -0400

    switch to classic GA

--- src/router/index.js
+++ src/router/index.js
@@ -130,6 +130,39 @@ const router = new Router({
   ]
 })
 
+if (process.env.NODE_ENV === 'production') {
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }
+    i[r].l = 1 * new Date()
+    a = s.createElement(o)
+    m = s.getElementsByTagName(o)[0]
+    a.async = 1
+    a.src = g
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+  /*
+  ** Set the current page
+  */
+  window.ga('create', 'UA-148021010-1', 'auto')
+  router.afterEach((to, from) => {
+    /*
+    ** We tell Google Analytics to add a `pageview`
+    */
+    window.ga('set', 'page', to.fullPath)
+    window.ga('send', 'pageview')
+  })
+}
+/*
+** Include Google Analytics Script
+*/
+
+/*
+** Every time the route changes (fired on initialization too)
+*/
+
 router.beforeEach((to, from, next) => {
   store.dispatch('pause_audio')
   next()
