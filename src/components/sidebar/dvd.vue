bd8532eefdbd2e9cf8cee152d503418c461ddc24
Nick Ford <nickforddesign@gmail.com>
2020-07-15 14:31:40 -0400

    update dvd link

--- src/components/sidebar/dvd.vue
+++ src/components/sidebar/dvd.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="module">
     <h3>Give Me The Banjo</h3>
-    <a href="http://www.newvideo.com/docurama/give-me-the-banjo/" target="_blank">
+    <a href="https://www.amazon.com/Give-Me-Banjo-Bela-Fleck/dp/B008AN827W" target="_blank">
       <img class="gallery-thumb" src="/static/dvd-cover.jpg" />
     </a>
     <p>
       <em>Give Me the Banjo</em>, narrated by Steve Martin and featuring Pete Seeger, Earl Scruggs, Bela Fleck, Rhiannon Giddens and The Carolina Chocolate Drops (84 mins., 2012). The national PBS documentary program with 30 mins of bonus content, including Uncle Dave Macon, Taj Mahal, Ralph Stanley and Cynthia Sayer.
     </p>
     <p>
-      To order, <a href="http://www.newvideo.com/docurama/give-me-the-banjo/" target="_blank">click here</a>.
+      To stream, <a href="https://www.amazon.com/Give-Me-Banjo-Bela-Fleck/dp/B008AN827W" target="_blank">click here</a>.
     </p>
   </div>
 </template>
