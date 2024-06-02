27ac8fdebd8257d43f45e4c2ab7b288bd59b4d68
Nick Ford <nfdev@gmail.com>
2024-06-02 16:13:07 -0400

    add prettier config

--- src/router/index.js
+++ src/router/index.js
@@ -1,94 +1,94 @@
-import Vue from "vue";
-import Router from "vue-router";
-import store from "@/store";
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from '@/store'
 
-Vue.use(Router);
+Vue.use(Router)
 
 const router = new Router({
-  mode: "history",
+  mode: 'history',
   routes: [
     {
-      path: "/",
-      name: "Intro",
+      path: '/',
+      name: 'Intro',
       component(resolve) {
-        require(["@/views/intro"], resolve);
+        require(['@/views/intro'], resolve)
       },
     },
     {
-      path: "/narratives",
-      name: "Narratives",
+      path: '/narratives',
+      name: 'Narratives',
       component(resolve) {
-        require(["@/views/narratives"], resolve);
+        require(['@/views/narratives'], resolve)
       },
     },
     {
-      path: "/narratives/:slug",
-      name: "Narrative",
+      path: '/narratives/:slug',
+      name: 'Narrative',
       component(resolve) {
-        require(["@/views/narratives/view"], resolve);
+        require(['@/views/narratives/view'], resolve)
       },
     },
     {
-      path: "/players",
-      name: "Players",
+      path: '/players',
+      name: 'Players',
       component(resolve) {
-        require(["@/views/players"], resolve);
+        require(['@/views/players'], resolve)
       },
       children: [
         {
-          path: ":slug",
-          name: "Player",
+          path: ':slug',
+          name: 'Player',
           component(resolve) {
-            require(["@/views/players/profile"], resolve);
+            require(['@/views/players/profile'], resolve)
           },
         },
       ],
     },
     {
-      path: "/maps",
-      name: "Maps",
+      path: '/maps',
+      name: 'Maps',
       component(resolve) {
-        require(["@/views/maps/index"], resolve);
+        require(['@/views/maps/index'], resolve)
       },
       children: [
         {
-          path: "/maps/region/:regionSlug",
-          name: "Map Region",
+          path: '/maps/region/:regionSlug',
+          name: 'Map Region',
           component(resolve) {
-            require(["@/views/maps/index"], resolve);
+            require(['@/views/maps/index'], resolve)
           },
         },
         {
-          path: "/maps/location/:slug",
-          name: "Map Marker",
+          path: '/maps/location/:slug',
+          name: 'Map Marker',
           component(resolve) {
-            require(["@/views/maps/content"], resolve);
+            require(['@/views/maps/content'], resolve)
           },
         },
       ],
     },
     {
-      path: "/timelines",
-      name: "Timelines",
+      path: '/timelines',
+      name: 'Timelines',
       component(resolve) {
-        require(["@/views/timelines/"], resolve);
+        require(['@/views/timelines/'], resolve)
       },
       children: [
         {
-          path: ":slug",
-          name: "Timeline",
+          path: ':slug',
+          name: 'Timeline',
           component(resolve) {
-            require(["@/views/timelines/view"], resolve);
+            require(['@/views/timelines/view'], resolve)
           },
         },
       ],
     },
     {
-      path: "/media",
-      name: "Media",
-      redirect: "/media/full-interviews",
+      path: '/media',
+      name: 'Media',
+      redirect: '/media/full-interviews',
       component(resolve) {
-        require(["@/views/media"], resolve);
+        require(['@/views/media'], resolve)
       },
       children: [
         // {
@@ -99,55 +99,55 @@ const router = new Router({
         //   }
         // },
         {
-          path: ":type",
-          name: "Media Type",
+          path: ':type',
+          name: 'Media Type',
           component(resolve) {
-            require(["@/views/media/list"], resolve);
+            require(['@/views/media/list'], resolve)
           },
         },
       ],
     },
     {
-      path: "/search",
-      name: "Search",
+      path: '/search',
+      name: 'Search',
       component(resolve) {
-        require(["@/views/search"], resolve);
+        require(['@/views/search'], resolve)
       },
     },
     {
-      path: "/search/:term",
-      name: "Search Results",
+      path: '/search/:term',
+      name: 'Search Results',
       component(resolve) {
-        require(["@/views/search"], resolve);
+        require(['@/views/search'], resolve)
       },
     },
     {
-      path: "/about",
-      name: "About",
+      path: '/about',
+      name: 'About',
       component(resolve) {
-        require(["@/views/about"], resolve);
+        require(['@/views/about'], resolve)
       },
     },
     {
-      path: "/help",
-      name: "Help",
+      path: '/help',
+      name: 'Help',
       component(resolve) {
-        require(["@/views/help"], resolve);
+        require(['@/views/help'], resolve)
       },
     },
     {
-      path: "/resources",
-      name: "Resources",
+      path: '/resources',
+      name: 'Resources',
       component(resolve) {
-        require(["@/views/resources"], resolve);
+        require(['@/views/resources'], resolve)
       },
     },
   ],
-});
+})
 
 router.beforeEach((to, from, next) => {
-  store.dispatch("pause_audio");
-  next();
-});
+  store.dispatch('pause_audio')
+  next()
+})
 
-export default router;
+export default router
